28aa3ffd08b85e2d2d84960b588e64451613b33a
Kusab <jakub@zero-85.pl>
2022-08-23 12:33:25 +0200

    Add Posts

---
+++ app/Nova/Post.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Nova;
+
+use Laravel\Nova\Fields\BelongsTo;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\Trix;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class Post extends Resource
+{
+    public static $model = \App\Models\Post::class;
+    public static $title = 'id';
+
+    public static function redirectAfterCreate(NovaRequest $request, $resource)
+    {
+        logger(__METHOD__.': '. var_export($request->query(), true));
+        return parent::redirectAfterCreate($request, $resource);
+    }
+
+
+    public static function redirectAfterUpdate(NovaRequest $request, $resource)
+    {
+        logger(__METHOD__.': '. var_export($request->query(), true));
+        return parent::redirectAfterUpdate($request, $resource);
+    }
+
+    public function fields(NovaRequest $request)
+    {
+        return [
+            ID::make()->sortable(),
+            Text::make('Title')->rules('required'),
+            Trix::make('Body')->rules('required'),
+            BelongsTo::make('User'),
+        ];
+    }
+
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +16,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $u = User::factory()->create([
+            'name'  => 'Test User',
+            'email' => 'test@example.com',
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Post::factory(10)->create(['user_id' => $u->id]);
     }
 }
