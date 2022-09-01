fa373e886d43bf6bdada381c54f14b9db549a8e4
Kusab <jakub@zero-85.pl>
2022-09-01 16:04:05 +0200

    Add dependency on User field

--- app/Nova/Post.php
+++ app/Nova/Post.php
@@ -3,9 +3,11 @@
 namespace App\Nova;
 
 use App\Models\Post as PostModel;
+use App\Models\User as UserModel;
 use App\Nova\Metrics\PostsPerStatus;
 use Laravel\Nova\Fields\BelongsTo;
 use Laravel\Nova\Fields\Date;
+use Laravel\Nova\Fields\FormData;
 use Laravel\Nova\Fields\ID;
 use Laravel\Nova\Fields\Select;
 use Laravel\Nova\Fields\Text;
@@ -22,7 +24,14 @@ class Post extends Resource
     {
         return [
             ID::make()->sortable(),
-            Text::make('Title')->rules('required'),
+            Text::make('Title')
+                ->rules('required')
+                ->dependsOn(['user'],
+                    function (Text $field, NovaRequest $request, FormData $formData) {
+                        logger("User changed to ".var_export($formData->user, true));
+                    }
+                ),
+
             Trix::make('Body')->rules('required'),
             BelongsTo::make('User'),
             Date::make('Created At')->filterable(),
