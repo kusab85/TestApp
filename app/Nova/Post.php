c36e6ee3371cc83a86b938e678fe0ad6e9f9e061
Kusab <jakub@zero-85.pl>
2022-12-28 12:24:07 +0100

    Add dependsOn it self to BelongsTo User field.

--- app/Nova/Post.php
+++ app/Nova/Post.php
@@ -6,6 +6,7 @@ use App\Models\Post as PostModel;
 use App\Nova\Metrics\PostsPerStatus;
 use Laravel\Nova\Fields\BelongsTo;
 use Laravel\Nova\Fields\Date;
+use Laravel\Nova\Fields\FormData;
 use Laravel\Nova\Fields\ID;
 use Laravel\Nova\Fields\Select;
 use Laravel\Nova\Fields\Text;
@@ -24,7 +25,15 @@ class Post extends Resource
             ID::make()->sortable(),
             Text::make('Title')->rules('required'),
             Trix::make('Body')->rules('required'),
-            BelongsTo::make('User'),
+            BelongsTo::make('User')
+                ->dependsOn(
+                    ['user'],
+                    function (BelongsTo $field, NovaRequest $request, FormData $formData) {
+                        $field->help(
+                            '$formData->user = '.var_export($formData->user, true).' '.
+                            '$field->value = '.var_export($field->value, true)
+                        );
+                    }),
             Date::make('Created At')->filterable(),
             Select::make('Status')
                 ->options(PostModel::availableStatuses(true))
